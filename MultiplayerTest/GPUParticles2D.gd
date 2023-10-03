4f7b79df7dd45cec48c390834f64b2f4488cad2f
Gfif <gfif90000@gmail.com>
2023-10-03 09:37:10 +0300

    Light on particles after fix

--- MultiplayerTest/GPUParticles2D.gd
+++ MultiplayerTest/GPUParticles2D.gd
@@ -3,13 +3,24 @@ extends GPUParticles2D
 
 # Called when the node enters the scene tree for the first time.
 var t = 0
+var maxt = 0
+var startScale = false
 func _ready():
 	emitting = true;
+	maxt = lifetime;
 	t = lifetime;
+	if(get_child_count() == 1):
+		startScale = get_child(0).scale
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if(get_child_count() == 1):
+		var stage = t/maxt
+		get_child(0).scale = stage * startScale
+		t-=delta
+		print(get_child(0).name)
+		pass
 	if(not emitting):
 		queue_free()
 	pass
